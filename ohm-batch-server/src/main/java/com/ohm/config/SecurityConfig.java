e97f6f29e5b6a51e97e6937c57f8001a2a7ba49d
kimyeongsol <onagu7167@gmail.com>
2023-04-01 09:53:59 +1100

    security

--- ohm-batch-server/src/main/java/com/ohm/config/SecurityConfig.java
+++ ohm-batch-server/src/main/java/com/ohm/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ohm.config;
 
+import com.ohm.handler.LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+//    private final LoginSuccessHandler loginSuccessHandler;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -19,11 +24,13 @@ public class SecurityConfig {
     }
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login").permitAll()
+        http.authorizeRequests().antMatchers("/login", "/aa").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login")
-                .usernameParameter("name")
+                .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("name")
+//                    .successHandler(loginSuccessHandler)
                 .and()
                 .logout();
 
